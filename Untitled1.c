4ca599b5bef49256131a6436eb38cb39cf0c3acc
Nim12334 <ninlin6859@gmail.com>
2026-06-07 16:10:57 +0300

    Add files via upload

--- Untitled1.c
+++ Untitled1.c
@@ -1,9 +1,85 @@
 #include <stdio.h>
-#include <limits.h>
-int main(){
-    printf("Char:min(%d),max(%d)\n",CHAR_MIN,CHAR_MAX);
-    printf("Int:min(%d),max(%d)\n",INT_MIN,INT_MAX);
-    printf("Short:min(%d),max(%d)\n",SHRT_MIN,SHRT_MAX);
-    printf("Long:min(%d),max(%d)\n",LONG_MIN,LONG_MAX);
-    printf("Long:min(0),max(%llu)\n",ULONG_MAX);
+void entab(){
+int c;
+int g=0;
+int g1=0;
+int indicator1;
+while((c=getchar())!=EOF){
+    if(c==32){
+        g++;
+    }
+    else if(c!=32){
+
+        g1++;
+        if(g1==8 || c==10){
+            g1=0;
+        }
+        if(g!=0){
+          g1--;
+          if(g1==8 || g1>8){
+            g1=0;
+          }
+
+          if(g==8-g1){
+
+            printf("%c",9);
+            g=0;
+            g1=0;
+
+          }
+          else if(g<8-g1){
+            for(int x=0;x<g;x++){
+                printf("%c",32);
+
+            }
+
+                g1+=g;
+                g1+=1;
+
+
+
+
+          g=0;
+
+          }
+          else if(g>8-g1){
+            printf("%c",9);
+            g-=(8-g1);
+            int g2=g/8;
+            for(int x=0;x<g2;x++){
+                printf("%c",9);
+            }
+            g-=(g2*8);
+            for(int x=0;x<g;x++){
+                printf("%c",32);
+            }
+            g1=g;
+            g1+=1;
+            g=0;
+
+          }
+
+        }
+        printf("%c",c);
+
+
+
+    }
+
+
+
+
+
 }
+
+
+}
+
+int main(){
+
+entab();
+
+}
+
+
+
