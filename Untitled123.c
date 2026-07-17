62ed7a2badbd348639ea5c56f7d24cb8fbc3cbf2
Nim12334 <ninlin6859@gmail.com>
2026-07-17 22:53:59 +0300

    Add files via upload

--- Untitled123.c
+++ Untitled123.c
@@ -1,36 +1,10 @@
 #include <stdio.h>
-void reverse(char s[]){
-int i=0;
-for(i=0;s[i]!=0;i++);
-char l[i];
-int i1=i;
-int i2=-1;
-while(i1>0){
-    i2++;
-    i1--;
-    l[i1]=s[i2];
-}
-for(int h=0;h<i;h++){
-    printf("%c",l[h]);
-}
-
-
-}
-
 int main(){
-    int c;
-    int h=0;
-    char jet[10000];
-    for(int h1=0;h1<10000;h1++){
+int g;
+char z;
+while((g=getchar())!=EOF){
+z=(g>=65&&g<=90)?g+32:g;
+putchar(z);
+}
 
-        jet[h1]=0;
-    }
-    while((c=getchar())!=EOF){
-            jet[h]=c;
-            h++;
-
-
-    }
-
-    reverse(jet);
 }
